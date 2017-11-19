0abd0bcc224bc6f168bb48118b1e62afca27918a
Joakim Svendsen <joakimbs@gmail.com>
2017-11-19 02:20:33 +0100

    Add test against simple CSV files

--- MergeCsv.Tests.ps1
+++ MergeCsv.Tests.ps1
@@ -13,12 +13,13 @@ Import-Module -Name MergeCsv -ErrorAction Stop
 
 # Doing this instead, at least for myself, to avoid having to copy files to
 # the profile/computer PowerShell modules directory each time...
-#Copy-Item -Path ..\MergeCsv.psm1 -Destination $PSScriptRoot
-#Move-Item -Path .\MergeCsv.psm1 -Destination "$PSScriptRoot\MergeCsv.ps1" -Force
+#Copy-Item -Path ..\MergeCsv.psm1 -Destination $PSScriptRoot\MergeCsv.ps1
+###Move-Item -Path .\MergeCsv.psm1 -Destination "$PSScriptRoot\MergeCsv.ps1" -Force
 #. "$PSScriptRoot\MergeCsv.ps1"
 
 Describe "Merge-Csv" {
-    It "Merges two simple CSV files / objects correctly" {
+    
+    It "Merges two simple objects with three IDs correctly" {
         $EmailObjects = @([PSCustomObject] @{
             Username = "John"
             Email = "john@example.com"
@@ -45,4 +46,33 @@ Describe "Merge-Csv" {
             '[{"Username":"Jane","Email":"jane@example.com","Department":"IT"},{"Username":"Janet","Email":"janet@maintexample.com","Department":"Maintenance"},{"Username":"John","Email":"john@example.com","Department":"HR"}]' |
             Should -Be $True
     }
+
+    It "Merges two simple CSV files with three IDs correctly" {
+        function InternalTestPathCSV {
+            [CmdletBinding()]
+            Param([String] $FilePath)
+            if (-not (Test-Path -Path "$PSScriptRoot\$FilePath" -PathType Leaf)) {
+                if (-not (Test-Path -Path "$PSScriptRoot\template-csvs\$FilePath" -PathType Leaf)) {
+                    throw "'$FilePath' isn't in the same directory as the test script or in a subfolder called 'template-csvs'."
+                }
+                else {
+                    "$PSScriptRoot\template-csvs\$FilePath"
+                }
+            }
+            else {
+                "$PSScriptRoot\$FilePath"
+            }
+        }
+        $FirstPath, $SecondPath = "simplecsv1.csv", "simplecsv2.csv" |
+            ForEach-Object {
+                InternalTestPathCSV -FilePath $_
+            }
+        Write-Verbose -Message "First path: $FirstPath. Second path: $SecondPath." #-Verbose
+        (Merge-Csv -Path $FirstPath, $SecondPath -Identity Username |
+            Sort-Object Username |
+            ConvertTo-Json -Depth 100 -Compress) -eq `
+            '[{"Username":"Jane","Email":"jane@example.com","Department":"IT"},{"Username":"Janet","Email":"janet@maintexample.com","Department":"Maintenance"},{"Username":"John","Email":"john@example.com","Department":"HR"}]' |
+            Should -Be $True
+    }
+
 }
